aabf0b1eb4b0571a7b756f42bbf3e49431cd737f
olga.jasionek <ogi22021996@gmail.com>
2023-06-29 11:07:13 +0200

    header-component

--- src/components/layout/header/header-component.tsx
+++ src/components/layout/header/header-component.tsx
@@ -1,9 +1,16 @@
+import styles from "./header-component.module.scss";
+
 const HeaderComponent = () => {
   return (
     <div className='container'>
-      <header>
-        <div>
+      <header className={styles.wrapper}>
+        <div className={styles.content}>
           <img src='./icons/logo.png' alt='logo' />
+          <div>
+            <div className={styles.search}>
+              <input type='text' placeholder='Co chcesz kupić?' />
+            </div>
+          </div>
         </div>
       </header>
     </div>
